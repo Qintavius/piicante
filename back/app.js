2495bdd823e96a5d16b9dc1b00ee926419a53381
Qintavius <qtzaba@gmail.com>
2022-10-12 14:20:38 +0200

    [ADD] Création des fichiers pour login et register users + install de bcrypt

--- back/app.js
+++ back/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const stuffRoutes = require('./routes/stuff')
+const stuffRoutes = require('./routes/stuff');
+const userRoutes= require('./routes/user');
 
 mongoose.connect('mongodb+srv://user-01-test:gafhHIXv9mDwpsrK@cluster0.te7xzeo.mongodb.net/?retryWrites=true&w=majority',
     {
@@ -24,5 +25,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
