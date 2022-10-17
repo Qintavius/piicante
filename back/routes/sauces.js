a3b1d8a475aa7ac6656f476c93b3fc341f17d8e5
Qintavius <qtzaba@gmail.com>
2022-10-17 15:12:04 +0200

    [UPDATE] Ajout de la route likeSauce

--- back/routes/sauces.js
+++ back/routes/sauces.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const auth = require('../middleware/auth');
 const router = express.Router();
+const multer = require('../middleware/multer-config');
 
 const saucesCtrl = require('../controllers/sauces');
 
+// Récupère tous les objets
+router.get('/', auth, saucesCtrl.getAllSauces);
+
 // Création nouvel objet
-router.post('/', auth, saucesCtrl.createSauce);
-
-// Modification objet
-router.put('/:id', auth, saucesCtrl.modifySauce);
-
-// Suppression objet
-router.delete('/:id', auth, saucesCtrl.deleteSauce);
+router.post('/', auth, multer, saucesCtrl.createSauce);
 
 // Récupère un objet
 router.get('/:id', auth, saucesCtrl.getOneSauce);
 
-// Récupère tous les objets
-router.get('/', auth, saucesCtrl.getAllSauces);
+// Modification objet
+router.put('/:id', auth, multer, saucesCtrl.modifySauce);
+
+// Suppression objet
+router.delete('/:id', auth, saucesCtrl.deleteSauce);
+
+// Like objet
+router.post('/:id/like', auth, saucesCtrl.likeSauce);
 
 module.exports = router;
